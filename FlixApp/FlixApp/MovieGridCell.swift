142aaec28f3f728b8d4c6c7f10319ca062f72a17
Nabil Fayak <72051470+NabilFayak@users.noreply.github.com>
2021-07-16 16:53:58 -0400

    Flix Part 2 Added

---
+++ FlixApp/FlixApp/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Nabil Fayak on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
